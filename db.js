3c2dec001d6d3a39736d62151b1a60341d9fa058
vedantkesharia <keshariavedant@gmail.com>
2023-02-21 18:36:47 +0530

    initial commit

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 
-//require("dotenv").config();
-//const new_uri = process.env.MONGO_URI;
-mongo_uri = "mongodb+srv://Vedant:tnadev1111@cluster0.ohbypjl.mongodb.net/inotebook?retryWrites=true&w=majority";
+require("dotenv").config();
+const new_uri = process.env.MONGO_URI;
+
 const connectToMongo = ()=>{
-    mongoose.connect(mongo_uri,()=>{
+    mongoose.connect(new_uri,()=>{
         console.log("Connected to Mongo successfully");
     })
 }
